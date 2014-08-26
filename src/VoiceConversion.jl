6c12810763ab490cb74522edb028e9f8ec887327
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-08-26 23:54:56 +0900

    Add logamp2mcep

--- src/VoiceConversion.jl
+++ src/VoiceConversion.jl
@@ -1,5 +1,10 @@
 module VoiceConversion
 
-# package code goes here
+# logamp2mcep converts log-amplitude spectrum to mel-cepstrum.
+function logamp2mcep(logamp::Vector{Float64}, order::Int, alpha::Float64)
+    ceps = real(ifft(logamp))
+    ceps[1] /= 2.0
+    return sptk.freqt(ceps, order, alpha)
+end
 
 end # module
