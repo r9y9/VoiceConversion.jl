f46be18f85a2bf7bca9a8b08f3123130566137b9
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-08-28 02:58:28 +0900

    Add show option to align.jl

--- src/scripts/align.jl
+++ src/scripts/align.jl
@@ -2,22 +2,26 @@ using DocOpt
 
 doc="""Align two mel-cesptrum sequences and create parallel data for training
 voice conversion models.
-b
+
 Usage:
     align.jl [options] <src_mcep> <tgt_mcep> <dst>
     align.jl --version
     align.jl -h | --help
 
 Options:
-    -h --help  show this message
+    -h --help   show this message
+    --show  show progress in graphical representation
 """
 
 using VoiceConversion
 using HDF5, JLD
-using PyPlot
+using PyCall
+
+@pyimport matplotlib.pyplot as plt
 
 function main()
     args = docopt(doc, version=v"0.0.1")
+    vis::Bool = args["--show"]
 
     src = load(args["<src_mcep>"])
     tgt = load(args["<tgt_mcep>"])
@@ -32,6 +36,15 @@ function main()
     # Alignment
     d = DTW(fstep=0, bstep=2) # allow one skip
     path = fit!(d, src_mcep, tgt_mcep)
+
+    # Visualize alignment result
+    if vis
+        plt.plot(path, linewidth=2.0, linestyle="--")
+        plt.xlabel("source speaker's index")
+        plt.ylabel("target speaker's index")
+        plt.title("estimated path")
+        plt.show()
+    end
 
     # create aligned tgt_mcep
     newtgt_mcep = zeros(eltype(src_mcep), size(src_mcep))
@@ -50,12 +63,14 @@ function main()
     # remove silence segment
     # TODO(ryuichi)
 
-
-    # plot (to be removed)
-    plot(src_mcep[1,:][:])
-    plot(newtgt_mcep[1,:][:])
-    sleep(3.0)
-    show()
+    # visualize time aligned mel-cesptrum
+    if vis
+        plt.plot(src_mcep[1,:][:], color="red", linewidth=1.5)
+        plt.plot(newtgt_mcep[1,:][:], color="blue", linewidth=1.5)
+        plt.xlabel("#frame")
+        plt.title("Time aligned mel-cesptrum (0-th order)")
+        plt.show()
+    end
 
     # save
     tgt["mcgram"] = newtgt_mcep
