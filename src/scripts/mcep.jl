220f9341635087b69677dce489e51069ff10976e
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-08-28 22:44:40 +0900

    Separate main to functions change key in JLD: "mcgram" -> "feature_matrix"

--- src/scripts/mcep.jl
+++ src/scripts/mcep.jl
@@ -20,19 +20,8 @@ using WORLD
 using WAV
 using HDF5, JLD
 
-function main()
-    args = docopt(doc, version=v"0.0.1")
-
-    x, fs = wavread(args["<input_audio>"], format="int")
-    @assert size(x, 2) == 1 "The input data must be monoral."
-    x = float64(x[:])
-    fs = int(fs)
-
-    period = float(args["--period"])
-    order = int(args["--order"])
-    alpha = float(args["--alpha"])
-    println("$(maximum(x)) and $(minimum(x))")
-
+function world_mcep(x, fs, period::Float64=5.0, order::Int=25,
+                    alpha::Float64=0.35)
     w = World(fs=fs, period=period)
 
     # Fundamental frequency (f0) estimation by DIO
@@ -43,7 +32,6 @@ function main()
 
     # Spectral envelope estimation
     spectrogram = cheaptrick(w, x, timeaxis, f0)
-    println(size(spectrogram))
 
     # Spectral envelop -> Mel-cesptrum
     mcgram = zeros(order+1, size(spectrogram, 1))
@@ -55,13 +43,31 @@ function main()
         mcgram[:,i] = logamp2mcep(logspec, order, alpha)
     end
 
+    return mcgram
+end
+
+function main()
+    args = docopt(doc, version=v"0.0.1")
+
+    x, fs = wavread(args["<input_audio>"], format="int")
+    @assert size(x, 2) == 1 "The input data must be monoral."
+    x = float64(x[:])
+    fs = int(fs)
+
+    period = float(args["--period"])
+    order = int(args["--order"])
+    alpha = float(args["--alpha"])
+
+    mcgram = world_mcep(x, fs, period, order, alpha)
+
     save(args["<dst>"],
+         "description", "WORLD-based Mel-cepstrum",
          "period", period,
          "fs", fs,
          "framelen", get_fftsize_for_cheaptrick(fs),
          "order", order,
          "alpha", alpha,
-         "mcgram", mcgram,
+         "feature_matrix", mcgram,
          )
 
     println("Dumped to ", args["<dst>"])
