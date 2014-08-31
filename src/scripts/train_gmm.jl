240d0a2b18932cffc4d0a7343c05aad7e5237c2d
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-08-31 16:49:43 +0900

    Save gmm parameters as column-major matrices

--- src/scripts/train_gmm.jl
+++ src/scripts/train_gmm.jl
@@ -34,29 +34,42 @@ function main()
     const min_covar::Float64 = float64(args["--min_covar"])
 
     dataset = ParallelDataset(args["<parallel_dir>"],
-                              joint=true, 
+                              joint=true,
                               diff=diff,
                               nmax=nmax)
 
-    gmm = mixture.GMM(n_components=n_components, 
+    gmm = mixture.GMM(n_components=n_components,
                       covariance_type="full",
                       n_iter=n_iter,
                       n_init=n_init,
                       min_covar=min_covar
                       )
-    
+
     println(gmm)
 
     # pass transposed matrix because python is row-major language
     @time gmm[:fit](dataset.X')
+
+    # save transposed parameters because julia is column-major language
+    # convert means
+    py_means = gmm[:means_]
+    means = py_means'
+
+    # convert covar tensor
+    py_covars = gmm[:covars_] # shape: (n_components, order, order)
+    order = size(py_covars, 2)
+    covars = Array(eltype(py_covars), order, order, n_components)
+    for m=1:n_components
+        covars[:,:,m] = reshape(py_covars[m,:,:], order, order)
+    end
 
     save(args["<dst_jld>"],
          "description", "Parameters of Gaussian Mixture Model",
          "diff", diff,
          "n_components", n_components,
          "weights", gmm[:weights_],
-         "means", gmm[:means_],
-         "covars", gmm[:covars_]
+         "means", means,
+         "covars", covars
          )
 end
 
