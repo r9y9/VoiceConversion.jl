98d2c56a56b359b4b74a06dd49501539b5395227
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-09-15 16:19:00 +0900

    Add push_delta

--- src/VoiceConversion.jl
+++ src/VoiceConversion.jl
@@ -7,7 +7,7 @@ export DTW, fit!, update!, set_template!, backward
 export logamp2mcep, mcep2e, world_mcep, align_mcep
 
 # Datasets
-export ParallelDataset
+export ParallelDataset, push_delta
 
 # Feature conversion
 export Converter, FrameByFrameConverter, TrajectoryConverter,
