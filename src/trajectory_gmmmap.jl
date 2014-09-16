1f7ba552c6914b88d074b3790bfd4f24f74742c6
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-09-16 22:12:37 +0900

    now work trajectory-based parameter conversion fix many bugs.. add assertions

--- src/trajectory_gmmmap.jl
+++ src/trajectory_gmmmap.jl
@@ -8,7 +8,7 @@ type TrajectoryGMMMap <: TrajectoryConverter
 
     # TODO consider gloval variance (gv)
     function TrajectoryGMMMap(gmmmap::GMMMap, T::Int)
-        const D = div(size(gmmmap.src_means, 1), 4)
+        const D = div(size(gmmmap.src_means, 1), 2)
         W = construct_weight_matrix(D, T)
         new(gmmmap, T, D, W)
     end
@@ -32,13 +32,17 @@ function construct_weight_matrix(D::Int, T::Int)
         W[2*D*(t-1)+1:2*D*t,:] = [w0, w1]
     end
 
+    @assert issparse(W)
+    @assert size(W) == (2*D*T, D*T)
+
     return W
 end
 
 function fvconvert(tgmm::TrajectoryGMMMap, X::Matrix{Float64})
-    const D, T = div(size(X,1),2), size(X,2)
     # input feature vector must contain delta feature
-    @assert D == tgmm.D*2
+    const D, T = div(size(X,1),2), size(X,2)
+    # TODO throw or error
+    @assert D == tgmm.D
     
     if T != tgmm.T
         tgmm.W = construct_weight_matrix(D, T)
@@ -55,24 +59,38 @@ function fvconvert(tgmm::TrajectoryGMMMap, X::Matrix{Float64})
         E[:,t] = tgmm.gmmmap.tgt_means[:,m] + tgmm.gmmmap.covarYX_XXinv[:,:,m] * 
             (X[:,t] - tgmm.gmmmap.src_means[:,m])
     end
+    E = vec(E)
+    @assert size(E) == (2*D*T,)    
 
     # Compute D^-1 eq.(41)
-    # TODO work
-    Dinv = zeros(2*D, 2*D, T)
+    Dinv = spzeros(2*D, 2*D)
     for t=1:T
         m = int(optimum_mix[t])
-        Dinv[:,:,t] = tgmm.gmmmap.covarYY[:,:,m] - tgmm.gmmmap.covarYX_XXinv[:,:,m] * 
+        dinv = tgmm.gmmmap.covarYY[:,:,m] - tgmm.gmmmap.covarYX_XXinv[:,:,m] * 
             tgmm.gmmmap.covarXY[:,:,m]
-        Dinv[:,:,t] = sparse(Dinv[:,:,t]^-1)
+        dinv = dinv^-1
+        if t == 1
+            Dinv[:,:] = sparse(dinv)
+        else
+            Dinv = blkdiag(Dinv, sparse(dinv))
+        end
     end
-    Dinv = blkdiag(Dinv)
+    # TODO: how can i pass diags as varargs to blkdiag..?
+    # Dinv = blkdiag([Dinv[:,:,t] for t=1:T])
+
     @assert size(Dinv) == (2*D*T, 2*D*T)
     @assert issparse(Dinv)
 
-    # Compute target static features
+    # Compute target static feature vector
     # eq.(39)
-    y = (W' * Dinv * W)^-1 * W' * Dinv * E 
-    @assert size(y) == (D, T)
+    W = tgmm.W # short alias
+    Wt_Dinv = W' * Dinv
+    @assert issparse(Wt_Dinv)
+    y = full(Wt_Dinv * W)^-1 * Wt_Dinv * E
+    @assert size(y) == (D*T,)
+
+    # Finally we get static feature vector
+    y = reshape(y, D, T)
 
     return y
 end
