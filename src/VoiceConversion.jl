0e8d6e0b1ec5c164b7e85b680ebc6934e585daf1
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-10-05 02:02:50 +0900

    Cleanup VoiceConversion.jl

--- src/VoiceConversion.jl
+++ src/VoiceConversion.jl
@@ -1,5 +1,13 @@
 module VoiceConversion
 
+# Voice conversion
+export FrameByFrameConverter, TrajectoryConverter,
+       fvconvert, vc,
+       GMMMap, TrajectoryGMMMap, TrajectoryGMMMapWithGV
+
+# Post filters
+export PeseudoGV, fvpostf!, fvpostf
+
 # Dynamic Time Warping (DTW) related functions
 export DTW, fit!, update!, set_template!, backward
 
@@ -9,29 +17,21 @@ export logamp2mcep, mcep2e, world_mcep, align_mcep
 # Datasets
 export ParallelDataset, GVDataset, push_delta
 
-# Feature conversion
-export Converter, FrameByFrameConverter, TrajectoryConverter,
-       GMMMap, GMM, GaussianMixtureModel, fvconvert, vc,
-       TrajectoryGMMMap, TrajectoryGMMMapWithGV
-
-# Post filters
-export PeseudoGV, fvpostf!, fvpostf
-
 ## Type Hierarchy ##
 abstract Converter
 abstract FrameByFrameConverter <: Converter
 abstract TrajectoryConverter <: Converter
 
-for fname in ["align.jl",
-              "dtw.jl",
-              "datasets.jl",
-              "mcep.jl",
-              "gmm.jl",
-              "gmmmap.jl",
-              "trajectory_gmmmap.jl",
-              "peseudo_gv.jl",
-              "converter.jl"]
-    include(fname)
+for fname in ["align",
+              "dtw",
+              "datasets",
+              "mcep",
+              "gmm",
+              "gmmmap",
+              "trajectory_gmmmap",
+              "peseudo_gv",
+              "converter"]
+    include(string(fname, ".jl"))
 end
 
 end # module
