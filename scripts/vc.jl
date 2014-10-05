b20a2a7b5b6f7f28fb026702e24f01d3be626515
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-10-05 18:08:52 +0900

    Add WORLD-based voice conversion script

---
+++ scripts/vc.jl
@@ -0,0 +1,95 @@
+using DocOpt
+
+doc="""Voice conversion based on WORLD-based speech analysis and 
+synthesis framework
+
+Usage:
+    vc.jl [options] <input_wav> <model_jld> <dst_wav>
+    vc.jl --version
+    vc.jl -h | --help
+
+Options:
+    -h --help        show this message
+    --period=PERIOD  frame period in msec [default: 5.0]
+    --order=ORDER    order of mel cepsrum [default: 25]
+    --alpha=ALPHA    all-pass constant [default: 0.0]
+    --trajectory     trajectory-based parameter conversion
+"""
+
+using VoiceConversion
+using MCepAlpha
+using WAV
+using SPTK
+using HDF5, JLD
+using WORLD
+
+function main()
+    args = docopt(doc, version=v"0.0.1")
+
+    x, fs = wavread(args["<input_wav>"], format="int")
+    @assert size(x, 2) == 1 "The input data must be monoral."
+    x = float64(x[:])
+    const fs = int(fs)
+    println("length of input signal is $(length(x)/fs) sec.")
+    
+    const period = float(args["--period"])
+    const order = int(args["--order"])
+    alpha = float(args["--alpha"])
+    if alpha == 0.0
+        alpha = mcepalpha(fs)
+    end
+    const trajectory = args["--trajectory"]
+        
+    # Load mapping model
+    gmm = load(args["<model_jld>"])
+    gmm["diff"] && error("not supported")
+
+    mapper = GMMMap(gmm)
+    if trajectory
+        mapper = TrajectoryGMMMap(mapper, 70)
+    end
+
+    elapsed_fe = @elapsed begin
+        w = World(fs=fs, period=period)
+        
+        # Fundamental frequency (f0) estimation by DIO
+        f0, timeaxis = dio1(w, x)
+        
+        # F0 re-estimation by StoneMask
+        f0 = stonemask(w, x, timeaxis, f0)
+        
+        # Spectral envelope estimation
+        spectrogram = cheaptrick(w, x, timeaxis, f0)
+
+        # Spectral envelop -> Mel-cesptrum
+        src = wsp2mc(spectrogram, order, alpha)
+
+        # aperiodicity ratio estimation
+        ap = aperiodicityratio(w, x, f0, timeaxis)
+    end
+
+    println("elapsed time in feature extraction is $(elapsed_fe) sec.")
+    if trajectory
+        # add delta feature
+        src = [src[1,:], push_delta(src[2:end,:])]
+    end
+    
+    # Perform conversion
+    elapsed_vc = @elapsed converted = vc(mapper, src)
+    println("elapsed time in conversion process is $(elapsed_vc) sec.")
+    
+    # Mel-Cepstrum to spectrum
+    converted_spectrogram = mc2wsp(converted, size(spectrogram,1), -alpha)
+
+    # Waveform synthesis using WORLD
+    elapsed_syn = @elapsed begin
+        y = synthesis_from_aperiodicity(w, f0, converted_spectrogram, ap,
+                                        length(x))
+    end
+    println("elapsed time in waveform synthesis is $(elapsed_syn) sec.")
+    
+    wavwrite(float32(y/32768), args["<dst_wav>"], Fs=fs)
+    println("Dumped to ", args["<dst_wav>"])
+end
+
+@time main()
