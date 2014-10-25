75c707c6d5c6e0cc54144500a39e22eb88ddcf9d
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-10-25 18:01:10 +0900

    resolve dependencies with non-official packages when Pkg.build

---
+++ deps/build.jl
@@ -0,0 +1,17 @@
+# resolve dependencies with non-officlal packages
+
+function install_non_official_pkg(path; build::Bool=false)
+    name = basename(splitext(path)[1])
+    try
+        Pkg.installed(name)
+    catch
+        Pkg.clone(path)
+        if build
+            Pkg.build(name)
+        end
+    end
+end
+
+install_non_official_pkg("https://github.com/r9y9/SPTK.jl", build=true)
+install_non_official_pkg("https://github.com/r9y9/WORLD.jl", build=true)
+install_non_official_pkg("https://github.com/r9y9/MCepAlpha.jl")
