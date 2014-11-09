b58c832726036fb025e81bfc80da67957a759129
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-11-09 19:45:26 +0900

    fix wrong names

--- test/vc.jl
+++ test/vc.jl
@@ -71,7 +71,7 @@ function trajectory_vc_clb2slt()
     # Load GMM to convert speech signal of `clb` to that of `slt`,
     # mixture: 32, order of mel-cepstrum: 40+40 (with delta feature)
     modelpath = joinpath(Pkg.dir("VoiceConversion"), "test", "model",
-                         "clb_to_slt_gmm32_order40_with_delta.jld")
+                         "clb_and_slt_gmm32_order40_with_delta.jld")
     gmm = load(modelpath)
     @assert !gmm["diff"]
 
@@ -84,4 +84,4 @@ end
 ### Tests
 
 vc_clb2slt()
-trajectory_diffvc_clb2slt()
+trajectory_vc_clb2slt()
